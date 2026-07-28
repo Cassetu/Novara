12c191e2fd98646ac9191be1f8fc89f4b9e8bf42
Cassetu <cyrusbachmann@gmail.com>
2026-07-28 08:36:51 -0700

    v1.04.00 7-28-26
    Notes: Debug and implement

--- js/utils/dialogueBox.js
+++ js/utils/dialogueBox.js
@@ -10,17 +10,17 @@ export function createDialogueBox(config) {
     container.style.cssText = "border: 3px solid var(--border); box-shadow: 6px 6px 0px var(--border);margin:10px; padding:10px; min-height:122px; box-sizing:border-box;";
     container.innerHTML = `
         <div style="display: flex; gap: 15px; justify-content:space-between; align-items: flex-start;">
-            <img src="${imageSrc}" alt="${name} style="max-height:107px;border:3px solid var(--border);flex-shrink: 0;">
-            <div style="flex-grow:1; font-family: monospace;">
+            <img src="${imageSrc}" alt="${name}" style="max-height:107px;border:3px solid var(--border);flex-shrink: 0;">
+            <div style="flex-grow:1; font-family: monospace;display:flex; flex-direction:column;">
                 <strong style="display:block; color:var(--text-main); font-size: 24px;margin-bottom:4px;">${name}</strong>
-                <p style="color: var(--text-main); margin:0; line-height:1.4;">${texts[0]}</p>
-                <button id="mayor-next-btn" style="align-self:flex-end;margin:10px;padding:4px 12px; font-family: monospace; font-weight:bold; cursor: pointer; border: 2px solid var(--border);color: var(--text-main);">Next</button>
+                <p class="dialogue-text" style="color: var(--text-main); margin:0; line-height:1.4;">${texts[0]}</p>
+                <button class="dialogue-next-btn" style="align-self:flex-end;margin:10px;padding:4px 12px; font-family: monospace; font-weight:bold; cursor: pointer; border: 2px solid var(--border);color: var(--text-main);">Next</button>
             </div>
         </div>
     `;
 
-    const textEl = container.querySelector(".dialogueText");
-    const nextBtn = container.querySelector(".dialogue-next-btn");;
+    const textEl = container.querySelector(".dialogue-text");
+    const nextBtn = container.querySelector(".dialogue-next-btn");
 
     if (texts.length <= 1) {
         nextBtn.innerText = "Done"
